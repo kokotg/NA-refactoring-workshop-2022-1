4aad31d3db93d790e5428b5ff96212fe3884686a
Milos Radjenovic <milos.radjenovic@gmail.com>
2022-07-14 16:17:19 +0200

    CCN down to 19

--- SnakeController/SnakeController.cpp
+++ SnakeController/SnakeController.cpp
@@ -63,37 +63,69 @@ Controller::Controller(IPort& p_displayPort, IPort& p_foodPort, IPort& p_scorePo
     }
 }
 
+bool Controller::CheckLoseConditions(Segment newHead){
+    bool lost = false;
+
+    if(newHead.x < 0 or newHead.y < 0 or
+            newHead.x >= m_mapDimension.first or newHead.y >= m_mapDimension.second){
+                lost = true;
+            }
+    if(not lost){
+        for (auto segment : m_segments) {
+            if (segment.x == newHead.x and segment.y == newHead.y) {
+                lost = true;
+                break;
+            }
+        }
+    }
+
+    if(lost){
+        m_scorePort.send(std::make_unique<EventT<LooseInd>>());
+    }
+    
+    return lost;
+}
+
+void Controller::UpdateSnakePosition(Segment newHead){
+    m_segments.push_front(newHead);
+            DisplayInd placeNewHead;
+            placeNewHead.x = newHead.x;
+            placeNewHead.y = newHead.y;
+            placeNewHead.value = Cell_SNAKE;
+
+            m_displayPort.send(std::make_unique<EventT<DisplayInd>>(placeNewHead));
+}
+
+
 void Controller::receive(std::unique_ptr<Event> e)
 {
     try {
         auto const& timerEvent = *dynamic_cast<EventT<TimeoutInd> const&>(*e);
 
+        // decide what is the current head
         Segment const& currentHead = m_segments.front();
 
+        // create new head object based on movement direction
+        int downDir = (m_currentDirection & 0b10) ? 1 : -1;
         Segment newHead;
-        newHead.x = currentHead.x + ((m_currentDirection & 0b01) ? (m_currentDirection & 0b10) ? 1 : -1 : 0);
-        newHead.y = currentHead.y + (not (m_currentDirection & 0b01) ? (m_currentDirection & 0b10) ? 1 : -1 : 0);
+        newHead.x = currentHead.x + ((m_currentDirection & 0b01) ? downDir : 0);
+        newHead.y = currentHead.y + (not (m_currentDirection & 0b01) ? downDir : 0);
         newHead.ttl = currentHead.ttl;
 
+
+        //Check lose condition
         bool lost = false;
+        
+        lost = CheckLoseConditions(newHead);
+        
+        
+        
 
-        for (auto segment : m_segments) {
-            if (segment.x == newHead.x and segment.y == newHead.y) {
-                m_scorePort.send(std::make_unique<EventT<LooseInd>>());
-                lost = true;
-                break;
-            }
-        }
-
-        if (not lost) {
+        if(not lost) {
+            //check got food
             if (std::make_pair(newHead.x, newHead.y) == m_foodPosition) {
                 m_scorePort.send(std::make_unique<EventT<ScoreInd>>());
                 m_foodPort.send(std::make_unique<EventT<FoodReq>>());
-            } else if (newHead.x < 0 or newHead.y < 0 or
-                       newHead.x >= m_mapDimension.first or
-                       newHead.y >= m_mapDimension.second) {
-                m_scorePort.send(std::make_unique<EventT<LooseInd>>());
-                lost = true;
             } else {
                 for (auto &segment : m_segments) {
                     if (not --segment.ttl) {
@@ -106,24 +138,20 @@ void Controller::receive(std::unique_ptr<Event> e)
                     }
                 }
             }
-        }
 
-        if (not lost) {
-            m_segments.push_front(newHead);
-            DisplayInd placeNewHead;
-            placeNewHead.x = newHead.x;
-            placeNewHead.y = newHead.y;
-            placeNewHead.value = Cell_SNAKE;
-
-            m_displayPort.send(std::make_unique<EventT<DisplayInd>>(placeNewHead));
+            UpdateSnakePosition(newHead);
+            
 
             m_segments.erase(
-                std::remove_if(
-                    m_segments.begin(),
-                    m_segments.end(),
-                    [](auto const& segment){ return not (segment.ttl > 0); }),
-                m_segments.end());
+            std::remove_if(
+            m_segments.begin(),
+            m_segments.end(),
+            [](auto const& segment){ return not (segment.ttl > 0); }),
+            m_segments.end());
+
+
         }
+
     } catch (std::bad_cast&) {
         try {
             auto direction = dynamic_cast<EventT<DirectionInd> const&>(*e)->direction;
